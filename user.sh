e94e726464dd9e11c43d3dfabceb175369ef3a7a
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-07-18 13:11:54 +0530

    modified user.sh file

--- user.sh
+++ user.sh
@@ -2,24 +2,22 @@
 
 DATE=$(date +%F)
 LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+SCRIPT_NAME=$(basename $0)
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ];
-then
+if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
@@ -27,74 +25,62 @@ VALIDATE(){
     fi
 }
 
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$LOGFILE
-VALIDATE $? "Setting up NPM Source"
+# Setup NodeJS
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+VALIDATE $? "Setting up NodeJS repo"
 
 yum install nodejs -y &>>$LOGFILE
-VALIDATE $? " installing node js "
+VALIDATE $? "Installing NodeJS"
 
-
+# Add user roboshop if not exists
 id roboshop &>>$LOGFILE
 if [ $? -ne 0 ]; then
     useradd roboshop &>>$LOGFILE
-    VALIDATE $? "Adding user roboshop"
+    VALIDATE $? "Creating roboshop user"
 else
     echo -e "User roboshop already exists ... $Y SKIPPING $N"
 fi
 
-if [ ! -d /app ]; then
-    mkdir /app &>>$LOGFILE
-    VALIDATE $? "creating app directory"
-else
-    echo -e "/app already exists ... $Y SKIPPING $N"
-fi
-
-
-curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>>$LOGFILE
-VALIDATE $? "downloading user artifact"
-
+# Create /app directory if not exists
 if [ ! -d /app ]; then
     mkdir /app &>>$LOGFILE
     VALIDATE $? "Creating /app directory"
 else
-    echo -e "/app already exists ... $Y SKIPPING $N"
+    echo -e "/app directory already exists ... $Y SKIPPING $N"
 fi
 
-VALIDATE $? "moving into app "
+# Download and extract application
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>>$LOGFILE
+VALIDATE $? "Downloading user artifact"
 
-unzip /tmp/user.zip &>>$LOGFILE
-VALIDATE $? " unzipping user"
+cd /app &>>$LOGFILE
+VALIDATE $? "Moving into app directory"
 
-npm install  &>>$LOGFILE
-VALIDATE $? "installing npm"
+unzip -o /tmp/user.zip &>>$LOGFILE
+VALIDATE $? "Unzipping user"
 
-cp /home/centos/roboshop-shell/user.service  /etc/systemd/system/user.service &>>$LOGFILE
-VALIDATE $? "copying user.service"
+npm install &>>$LOGFILE
+VALIDATE $? "Installing dependencies"
 
+# Set up systemd service
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>$LOGFILE
+VALIDATE $? "Copying user.service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? " deaon reaload"
+VALIDATE $? "Daemon reload"
 
 systemctl enable user &>>$LOGFILE
-VALIDATE $? "enabling user"
- 
-systemctl start user &>>$LOGFILE
-VALIDATE $? "starting user"
+VALIDATE $? "Enabling user service"
 
+systemctl start user &>>$LOGFILE
+VALIDATE $? "Starting user service"
+
+# MongoDB setup
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
-VALIDATE $? "Copying mongo repo"
+VALIDATE $? "Copying mongo.repo"
 
 yum install mongodb-org-shell -y &>>$LOGFILE
-VALIDATE $? "installing mongodb org shell"
+VALIDATE $? "Installing MongoDB client"
 
 mongo --host mongodb.janadevops.fun </app/schema/user.js &>>$LOGFILE
-VALIDATE $? "loading user data into mongodb"
-
-
-
-
-
-
-
-
+VALIDATE $? "Loading user data into MongoDB"
